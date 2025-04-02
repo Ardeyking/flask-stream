d16755be586c7d31e2a998c73d54719a4e634db5
Ardeyking <traks1299@gmail.com>
2025-04-02 16:11:32 +0100

    Fix video_feed timeout using gevent and placeholder frame

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, Response, request, render_template
 import time
 
 app = Flask(__name__)
-latest_frame = b''  # Stores the most recent uploaded frame
+latest_frame = b''  # Stores the most recent uploaded JPEG
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # This should load your live viewer page
+    return render_template('index.html')  # Page with <img src="/video_feed">
 
 @app.route('/upload_frame', methods=['POST'])
 def upload_frame():
@@ -19,9 +19,12 @@ def video_feed():
     def generate():
         global latest_frame
         while True:
-            if latest_frame:
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + latest_frame + b'\r\n')
-            time.sleep(0.05)  # Stream at ~20 FPS max
+            # If no frame yet, send a tiny blank JPEG
+            frame = latest_frame if latest_frame else b'\xff\xd8\xff\xd9'
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+            time.sleep(0.05)  # ~20 FPS
 
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
